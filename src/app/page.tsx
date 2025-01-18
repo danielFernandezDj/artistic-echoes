46f21b2e7087102e7f4d348de5334f389098ff78
Daniel Fernandez <mdanif9728@gmail.com>
2025-01-17 22:18:39 -0800

    Add a h2 to the Herosection and fix the text-size.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ const Explore: React.FC = (() => {
   return (
     <div>
       <main className="flex flex-col w-full h-auto">
-        <section className="flex flex-col justify-center items-center w-svh w-full h-[30rem] text-center">
-          <h1 className="font-bold text-9xl text-gradient leading-none">
-            Print your Favorite!
+        <section className="flex flex-col justify-center items-center w-svh w-full my-12 md:my-28 text-center">
+          <h1 className="font-bold text-[min(15vw,280px)] text-gradient leading-none">
+            Free for all!
           </h1>
+          <h2 className="font-medium text-[min(5vw,55px)] text-gradient m-auto max-w-screen-xl">
+            Download you favorite image with not Copyright.
+          </h2>
         </section>
-
         <GalleryPage />
       </main>
     </div>
