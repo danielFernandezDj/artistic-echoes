2a0a1860329c22620a0cc836effd9add39227cc0
Daniel Fernandez <mdanif9728@gmail.com>
2025-03-18 16:16:32 -0700

    Change the NavAlertContent.tsx style.

--- src/components/layout/NavAlertContent.tsx
+++ src/components/layout/NavAlertContent.tsx
@@ -1,25 +1,42 @@
 "use client"
 
-import { AlertDialog, Flex } from "@radix-ui/themes"
+import { AlertDialog, Flex, Button } from "@radix-ui/themes"
 import { useAuthModal } from "@/context/AuthModalContext"
 import SignIn from "@/components/layout/SignIn"
 import { X } from "lucide-react"
+import { useSession, signOut } from "next-auth/react";
 
 export default function NavAlertContent() {
     const { closeModal } = useAuthModal()
+    const { data: session } = useSession()
+
     return (
         <>
-            <AlertDialog.Content maxWidth="400px">
-                <Flex className='mb-4' justify="between" align="center">
+            <AlertDialog.Content className="flex flex-col gap-4">
+                <Flex justify="between" align="center">
                     <AlertDialog.Title>
-                        Sign In
+                        <span className="text-base font-normal"> Welcome </span> <br />
+                        <span className="text-magenta-color">
+                            {session?.user?.name}
+                        </span>
                     </AlertDialog.Title>
-                    <AlertDialog.Cancel
-                        onClick={closeModal}
-                        className="text-red-500 relative bottom-2 cursor-pointer hover:rotate-90 transition ease-in-out duration-300"
-                    >
-                        <X size={32} />
-                    </AlertDialog.Cancel>
+
+                    <div className="flex  gap-6">
+
+                        <Button
+                            color="red"
+                            onClick={() => signOut()}
+                        >
+                            Sign out
+                        </Button>
+
+                        <AlertDialog.Cancel
+                            onClick={closeModal}
+                            className="text-red-500 relative cursor-pointer hover:rotate-90 transition ease-in-out duration-300"
+                        >
+                            <X size={32} />
+                        </AlertDialog.Cancel>
+                    </div>
                 </Flex>
 
                 <SignIn />
--- src/components/layout/UserMenu.tsx
+++ src/components/layout/UserMenu.tsx
@@ -7,15 +7,10 @@ export default function UserMenu() {
     const { data: session } = useSession();
 
     return (
-        <div className="flex flex-col bg-white p-8 rounded-lg shadow-md w-96 border-2">
-            <h2 className="text-2xl font-bold mb-6 text-center">Welcome</h2>
-
-            <div className="border-2">
-                <Text>You are signed as <span className="text-green-500">{session?.user?.name}</span>.</Text> <br />
-                <Text>You are using <span className="text-blue-500">{session?.user?.email}</span>.</Text>
-                <Button color="red" mt={"2"} onClick={() => signOut()}>Sign out</Button>
-            </div>
-
+        <div className="flex flex-col gap-2 bg-white p-8 rounded-lg shadow-md w-96">
+            <Text>
+               User content goes here.
+            </Text>
         </div>
     )
 }
