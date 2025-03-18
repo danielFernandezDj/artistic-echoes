033a501f792a99e4ce186a78b7b4818cb83db85e
Daniel Fernandez <mdanif9728@gmail.com>
2025-03-18 16:24:29 -0700

    Working in the UserMenu to show up the user favorites images.

--- src/components/layout/UserMenu.tsx
+++ src/components/layout/UserMenu.tsx
@@ -1,16 +1,18 @@
 "use client"
 
-import { useSession, signOut } from "next-auth/react";
-import { Button, Text } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
+import { Text } from "@radix-ui/themes";
 
 export default function UserMenu() {
     const { data: session } = useSession();
 
     return (
-        <div className="flex flex-col gap-2 bg-white p-8 rounded-lg shadow-md w-96">
+        <div className="flex flex-col gap-2 bg-white p-8 rounded-lg shadow-md">
             <Text>
                User content goes here.
             </Text>
+
+           
         </div>
     )
 }
