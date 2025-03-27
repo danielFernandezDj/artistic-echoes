3424f8d36e799e91b4792d5b7c80d7f99343791f
Daniel Fernandez <mdanif9728@gmail.com>
2025-03-27 16:33:46 -0700

    Update [ remove-user-image ] route to use museumID instead.

--- src/app/api/remove-user-image/route.ts
+++ src/app/api/remove-user-image/route.ts
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server";
 export async function DELETE(request: Request) {
   try {
     const body = await request.json();
-    const { userEmail, objectID } = body;
+    const { userEmail, museumID } = body;
 
-    if (!userEmail || !objectID) {
+    if (!userEmail || !museumID) {
       return NextResponse.json(
         { message: "Missing required fields" },
         { status: 400 }
@@ -22,7 +22,7 @@ export async function DELETE(request: Request) {
       return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
 
-    const updatedImages = user.imagesID.filter((img) => img !== objectID);
+    const updatedImages = user.imagesID.filter((img) => img !== museumID);
 
     if (updatedImages.length === user.imagesID.length) {
       return NextResponse.json(
