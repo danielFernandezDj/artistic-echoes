06e611358a3ee3c8ff8d3397435286f75f9ab29c
Daniel Fernandez <mdanif9728@gmail.com>
2025-03-30 06:17:23 -0700

    Successfuly fix the dimension and mobile style in the userImageView.

--- src/components/layout/UserContentMenu.tsx
+++ src/components/layout/UserContentMenu.tsx
@@ -5,12 +5,16 @@ import { useSession } from "next-auth/react";
 import { Text } from "@radix-ui/themes";
 import Image from "next/image";
 import { ImageStock } from "@/lib/ImageStockType";
+// import ImageView from "../ui/ImageView";
+import UserImageView from "../ui/UserImageView";
 
 export default function UserMenu() {
     const { data: session } = useSession();
     const [imageIDs, setImageIds] = useState<string[]>([])
     const [loading, setLoading] = useState<boolean>(true);
     const [images, setImages] = useState<ImageStock[]>([]);
+    const [imageView, setImageView] = useState<boolean>(false);
+    const [selectedImage, setSelectedImage] = useState<ImageStock | null>(null);
 
     useEffect(() => {
         const fetchImages = async () => {
@@ -59,27 +63,45 @@ export default function UserMenu() {
     }
 
     return (
-        <div className="flex flex-col gap-2">
-            {session && (
-                <div className="flex flex-col gap-4">
-                    <Text weight={"medium"}>
-                        Total: {imageIDs.length}
-                    </Text>
-                    <div className="columns-1 md:columns-2 lg:columns-3 gap-6 ">
-                        {images.map((image, index) => (
-                            <div key={index} className="flex items-center p-5 md:mb-5 md:p-0">
-                                <Image
-                                    src={image.primaryImageSmall}
-                                    alt={image.objectTitle || `Image ${index}`}
-                                    width={100}
-                                    height={100}
-                                    className="w-full h-auto transition-all duration-200 ease-in-out border-4 md:border-8 hover:scale-105 cursor-pointer"
-                                />
-                            </div>
-                        ))}
-                    </div>
-                </div>
+        <>
+            {imageView && selectedImage && (
+                <UserImageView
+                    imageView={imageView}
+                    setImageView={setImageView}
+                    selectedImage={selectedImage}
+                    setSelectedImage={setSelectedImage}
+                />
             )}
-        </div>
+
+            <div className="flex flex-col gap-2">
+                {session && (
+                    <div className="flex flex-col gap-4">
+                        <Text weight={"medium"}>
+                            Total: {imageIDs.length}
+                        </Text>
+                        <div className="columns-1 md:columns-2 lg:columns-3 gap-6 ">
+                            {images.map((image, index) => (
+                                <div
+                                    key={index}
+                                    onClick={() => {
+                                        setSelectedImage(image);
+                                        setImageView(true)
+                                    }}
+                                    className="flex items-center p-5 md:mb-5 md:p-0"
+                                >
+                                    <Image
+                                        src={image.primaryImageSmall}
+                                        alt={image.objectTitle || `Image ${index}`}
+                                        width={100}
+                                        height={100}
+                                        className="w-full h-auto transition-all duration-200 ease-in-out border-4 md:border-8 hover:scale-105 cursor-pointer"
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
+        </>
     )
 }
